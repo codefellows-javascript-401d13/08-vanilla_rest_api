42c47c31bdf31b57a077b37033e2e20d3740d2fb
Dana Kulp <dkulp23@gmail.com>
2017-02-22 17:42:21 -0800

    Added method for the server to handle get requests.

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 'use strict';
 
 const http = require('http');
+const Router = require('./lib/router.js');
+const Team = require('./model/team.js');
+const locker = require('./lib/team-storage.js');
 const PORT = process.env.PORT || 3000;
+const router = new Router();
 
-const server = http.createServer();
+router.get('/api/team', function(req, res) {
+  if (req.url.query.id) {
+    locker.fetchTeam('team', req.url.query.id)
+    .then( team => {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      res.write(JSON.stringify(team));
+      res.end();
+    })
+    .catch( err => {
+      console.error(err);
+      res.writeHead(404, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('team not found');
+      res.end();
+    });
+    return;
+  }
+
+  res.writeHead(400, {
+    'Content-Type': 'text/plain'
+  });
+  res.write('bad request');
+  res.end();
+});
+
+
+const server = http.createServer(Router.route());
 
 server.listen(PORT, () => {
   console.log(`Server is up on port: ${PORT}`);
