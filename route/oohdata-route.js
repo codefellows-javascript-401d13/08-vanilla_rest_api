2d4b2607b5e41f4fc647019c4da64ef90d5275cb
GLSea1979 <builtbygl@gmail.com>
2017-02-23 15:41:36 -0800

    built some stuff 2 of 7 test pass

---
+++ route/oohdata-route.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const storage = require('../lib/storage.js');
+const response = require('../lib/response.js');
+const Oohdata = require('../model/oohdata.js');
+
+module.exports = function(router) {
+  router.get('api/oohdata', function(req, res) {
+    if (req.url.query.id) {
+      storage.fetchItem('oohdata', req.url.query.id)
+      .then( oohdata => {
+        console.log('got it');
+        response.sendJSON(res, 200, oohdata);
+      })
+      .catch( err => {
+        console.log('nope');
+        console.error(err);
+        response.sendText(res, 404, 'not found');
+      });
+
+      return;
+    };
+    response.sendText(res, 400, 'bad request');
+  });
+
+  router.post('/api/oohdata', function(req, res) {
+    try {
+      var oohdata = new Oohdata(req.body.animal, req.body.story);
+      storage.createItem('oohdata', oohdata);
+      response.sendJSON(res, 200, oohdata);
+    } catch (err) {
+      console.error(err);
+      response.sendText(res, 400, 'bad request');
+    };
+  });
+
+}
