069598b44c1bc0216c7d7f79f6a134d1865e4ce4
noahgribbin <noahgribbin@yahoo.com>
2017-02-23 15:39:01 -0800

    unstashing

--- server.js
+++ server.js
@@ -1,59 +1,11 @@
 'use strict';
-
 const http = require('http');
 const Movie = require('./model/movie.js');
 const Router = require('./lib/router.js');
 const storage = require('./lib/storage.js');
 const PORT = process.env.PORT || 3000;
 const router = new Router();
-
-router.get('/api/movie', function(req, res) {
-  if (req.url.query.id) {
-    storage.fetchItem('movie', req.url.query.id)
-    .then( movie => {
-      res.writeHead(200, {
-        'Content-Type': 'application/json'
-      });
-      res.write(JSON.stringify(movie));
-      res.end();
-    })
-    .catch( err => {
-      console.error(err);
-      res.writeHead(404, {
-        'Content-Type': 'text/plain'
-      });
-      res.write('not found');
-      res.end();
-    });
-    return;
-  }
-
-  res.writeHead(400, {
-    'Content-Type': 'text/plain'
-  });
-  res.write('bad request');
-  res.end();
-});
-
-
-router.post('/api/movie', function(req, res) {
-  try {
-    var movie = new Movie(req.body.name, req.body.director);
-    storage.createItem('movie', movie);
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
-    });
-    res.write(JSON.stringify(movie));
-    res.end();
-  } catch (err) {
-    console.error(err);
-    res.writeHead(400, {
-      'Content-Type': 'text/plain'
-    });
-    res.write('bad request');
-    res.end();
-  }
-});
+require('./route/movie-route.js')(router);
 
 const server = http.createServer(router.route());
 
