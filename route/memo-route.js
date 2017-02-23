45b480069074313cadd4798914d704e2658dbd86
Jeremiah <jeremiaht.walters@gmail.com>
2017-02-23 12:52:02 -0800

    lab 9 test passing memos created

---
+++ route/memo-route.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const storage = require('../lib/storage');
+const response = require('../lib/response.js');
+const Memo = require('../model/memo.js');
+
+module.exports = function(router) {
+  router.get('/api/memo', function(req, res) {
+    if (req.url.query.id) {
+      storage.fetchItem('memo', req.url.query.id)
+            .then( memo => {
+              response.sendJSON(res, 200, memo);
+            })
+            .catch( err => {
+              response.sendText(res, 404, 'not found');
+            });
+      return;
+    };
+    response.sendText(res, 400, 'bad request');
+  });
+
+  router.post('/api/memo', function(req, res) {
+      try{
+          var memo = new Memo( req.body.author, req.body.entry);
+          storage.createItem('memo', memo);
+          response.sendJSON(res, 200, memo);
+      } catch (err) {
+          console.error(err)
+          response.sendText(res, 400, 'bad request');
+      }
+  });
+};
--- test/memo-route-test.js
+++ test/memo-route-test.js
@@ -13,64 +13,62 @@ describe('My Data Routes', function(){
       request.post('localhost:8000/api/memo')
      .send({ author: 'test author', entry: 'test entry' })
      .end((err, res) => {
-       if (err) return done(err);
        expect(res.status).to.equal(200);
        expect(res.body.author).to.equal('test author');
        expect(res.body.entry).to.equal('test entry');
        memo = res.body;
        done();
      });
-   });
- });
+    });
+  });
 
   describe('POST: api/memo', function() {
-   it('it should return a 400 error', function(done){
-     request.post('localhost:8000/api/memo')
+    it('it should return a 400 error', function(done){
+      request.post('localhost:8000/api/memo')
      .send({})
      .end((err, res) => {
        expect(res.status).to.equal(400);
        expect(res.text).to.equal('bad request');
        done();
-     })
-   });
- });
+     });
+    });
+  });
 
 
   describe('GET: /api/memo', function(){
     it('should return memo', function(done){
       request.get(`localhost:8000/api/memo?id=${memo.id}`)
      .end((err, res) => {
-       if (err) return done(err);
        expect(res.status).to.equal(200);
        expect(res.body.author).to.equal('test author');
-       expect(res.body.entry).to.equal('test entry');
-       done();
+       expect(res.body.entry).to.equal('test entry');  
      });
-   });
- });
+      done();
+    });
+  });
 
   describe('GET: /api/memo', function() {
-   it('should return a 400 error if no id was provided', function(done){
-     request.get('localhost:8000/api/memo')
+    it('should return a 400 error if no id was provided', function(done){
+      request.get('localhost:8000/api/memo')
      .end((err, res) => {
        expect(res.status).to.equal(400);
        expect(err).to.equal.null;
        expect(res.text).to.equal('bad request');
        done();
      });
-   });
- });
+    });
+  });
 
   describe('GET: /api/memo', function(){
-   it('should return a 404 not found', function(done){
-     request.get('localhost:8000/api/memo?id=123')
+    it('should return a 404 not found', function(done){
+      request.get('localhost:8000/api/memo?id=123')
      .end((err, res) => {
        expect(res.status).to.equal(404);
        expect(res.text).to.equal('not found');
-       done();
      });
-   });
- });
+      done();
+    });
+  });
 
 
 
