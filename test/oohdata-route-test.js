56a044ab2cb3b58814f993340f38c40ce156f779
GLSea1979 <builtbygl@gmail.com>
2017-02-22 18:10:05 -0800

    done, 7 passing

--- test/oohdata-route-test.js
+++ test/oohdata-route-test.js
@@ -9,7 +9,7 @@ describe('Oohdata Routes', function() {
   var oohdata = null;
 
   describe('POST: /api/oohdata', function() {
-    it('should return a oohdata', function(done) {
+    it('should return an oohdata', function(done) {
       request.post('localhost:3000/api/oohdata')
       .send({ animal: 'test animal', story: 'test story'})
       .end((err, res) => {
@@ -23,6 +23,18 @@ describe('Oohdata Routes', function() {
     });
   });
 
+  describe('POST: /api/oohdata', function() {
+    it('should return a 400 error', function(done) {
+      request.post('localhost:3000/api/oohdata')
+      .send({})
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal('bad request');
+        done();
+      })
+    })
+  })
+
   describe('GET: /api/oohdata', function() {
     it('should return a oohdata', function(done) {
       request.get(`localhost:3000/api/oohdata?id=${oohdata.id}`)
@@ -35,4 +47,28 @@ describe('Oohdata Routes', function() {
       });
     });
   });
+
+  describe('GET: /api/oohdata', function() {
+    it('should return a 400 error', function(done) {
+      request.get('localhost:3000/api/oohdata')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(err).to.equal.null;
+        expect(res.text).to.equal('bad request');
+        done();
+      });
+    });
+  });
+
+  describe('GET: /api/oohdata', function() {
+    it('should return a 404 not found', function(done) {
+      request.get('localhost:3000/api/oohdata?id=7272727')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(err).to.equal.null;
+        expect(res.text).to.equal('not found');
+        done();
+      })
+    })
+  })
 });
